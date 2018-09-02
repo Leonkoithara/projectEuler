20e13d287394debc44f1ed7c5577496857d7f482
Bharath Kumar <kumarbharath718@gmail.com>
2018-09-02 21:45:53 +0530

    update problem1

--- Problem1.c
+++ Problem1.c
@@ -1,21 +1,18 @@
 #include<stdio.h>
 
 int main(){
-	char ch;
-	do {
-		int i, sum = 0, size;
-		printf("\nSize: ");
-		scanf("%d", &size);
-		for(i=3;i<size;i+=3)
-			sum+=i;
-		for(i=5;i<size;i+=5)
-			sum+=i;
-		for(i=15;i<size;i+=15)
-			sum-=i;
-		printf("%d", sum);
-		printf("\nStill wanna continue? ");
-		scanf(" %c", &ch);
-	}while(ch=='y');
-
+	long int T, N, n;
+	scanf("%li", &T);
+	while(T--) {
+		long int sum = 0;
+		scanf("%li", &N);
+		n = (N-1)/3;
+		sum += (n)*(3+n*3) / (2);
+		n = (N-1)/5;
+		sum += (n)*(5+n*5) / (2);
+		n = (N-1)/15;
+		sum -= (n)*(15+n*15) / (2);
+		printf("%li\n", sum);
+	}
 	return 0;
 }
